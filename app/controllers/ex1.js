9776b392011bf03e1f69d93b0f06eb37ae057e63
Christien <21901129@etu.unicaen.fr>
2021-01-25 11:47:31 +0100

    ex1 - tout est affiché, il reste les interractions à faire

--- app/controllers/ex1.js
+++ app/controllers/ex1.js
@@ -1,4 +1,34 @@
 import Controller from '@ember/controller';
+import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
+
+let MAX = 100;
 
 export default class Ex1Controller extends Controller {
+  @tracked content = '';
+  @tracked info = '100';
+  @tracked enregistre;
+  @tracked visible = false;
+
+  @action
+  compte() {
+    this.info = MAX - this.content.valueOf().length;
+    this.visible = true;
+  }
+
+  @action
+  clear() {
+    this.content = '';
+    this.info = MAX;
+    this.visible = false;
+  }
+
+  @action
+  save() {
+    this.enregistre = !this.enregistre;
+    this.visible = true;
+  }
+
+  @action
+  update() {}
 }
--- app/routes/ex1.js
+++ app/routes/ex1.js
@@ -1,4 +1,10 @@
 import Route from '@ember/routing/route';
 
 export default class Ex1Route extends Route {
+  model() {
+    return {
+      message1: 'Note modifiée',
+      message2: 'Note sauvegardée',
+    };
+  }
 }
