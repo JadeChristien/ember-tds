86d6ce10654983ff2ed0fb54d28294524bd91c5f
Christien <21901129@etu.unicaen.fr>
2021-02-04 08:33:06 +0100

    td1 : récupération du this.content dans la route

--- app/routes/ex1.js
+++ app/routes/ex1.js
@@ -8,21 +8,26 @@ export default class Ex1Route extends Route {
   @action
   model(ex) {
     console.log('save');
-    var mot = 'aaaaaaa eeeeeee rt';
+
+    // Transformer le this.content en string
+    var mot = ex.toString();
+    // Séparer le content a chaque espace rencontré
     var reg = new RegExp(' ');
     this.tableau = mot.split(reg);
+    // Afficher le tableau dans la console pour regarder si tout est ok
     console.table(this.tableau);
-    console.table(ex);
 
+    // Voir le tableau dans la console => la variable tableau récupère bien le contenu de content
     var long = this.tableau.length;
     for (this.i = 0; this.i < long; this.i++) {
       console.log(this.tableau[this.i]);
     }
+
+    // Afficher une liste de tous les mots du content => ne marche pas
     return [
       { items: this.tableau[0] },
       { items: this.tableau[1] },
       { items: this.tableau[2] },
-      { items: this.tableau[3] },
     ];
   }
 
