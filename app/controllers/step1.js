2f6aa03284f39665f79408e9ee438e9d1b36138c
Christien <21901129@etu.unicaen.fr>
2021-02-17 14:40:13 +0100

    fin tp3

--- app/controllers/step1.js
+++ app/controllers/step1.js
@@ -4,38 +4,37 @@ import { action } from '@ember/object';
 
 export default class Step1Controller extends Controller {
   //éléments initiaux qui sont dans la 1ere liste
-  dispoItems=[];
+  dispoItems = [];
   //éléments initiaux qui sont dans la 2nd liste
-  includedItems=[];
+  includedItems = [];
   //éléments sélectionnés dans la 1ere liste
-  @tracked dispoItems_=[];
+  @tracked dispoItems_ = [];
   //éléments sélectionnés dans la 2nd liste
-  @tracked includedItems_=[];
+  @tracked includedItems_ = [];
 
   //Affecte elements à dispoItems_, action déclenchée sur le @onChange de la liste des dispoItems
   @action
-  changeDispo(elements){
-    this.dispoItems_=elements;
+  changeDispo(elements) {
+    this.dispoItems_ = elements;
   }
 
   //Affecte elements à includedItems_, action déclenchée sur le @onChange de la liste des includedItems
   @action
-  changeIncluded(elements){
-    this.includedItems_=elements;
+  changeIncluded(elements) {
+    this.includedItems_ = elements;
   }
 
   //Ajoute depuis source les éléments what à dest
   @action
-  add(source, dest, what){
+  add(source, dest, what) {
     dest.pushObjects(what);
     source.removeObjects(what);
   }
 
   //Retire depuis source les éléments what et les ajoute à dest
   @action
-  remove(source, dest, what){
+  remove(source, dest, what) {
     dest.pushObjects(what);
     source.removeObjects(what);
   }
-
 }
--- app/helpers/array-contains.js
+++ app/helpers/array-contains.js
@@ -1,8 +1,8 @@
 import { helper } from '@ember/component/helper';
 
-export default helper(function arrayContains(params/*, hash*/) {
+export default helper(function arrayContains(params /*, hash*/) {
   // Regarde si un élément appartient aux items sélectionnés de la liste
-  const [items, value,prop] = params;
-  let id=prop ||'id'
-  return items.filterBy(id,value).length > 0;
+  const [items, value, prop] = params;
+  let id = prop || 'id';
+  return items.filterBy(id, value).length > 0;
 });
