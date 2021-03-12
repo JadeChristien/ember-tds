5fe65dc49099e93a5605a5c18cb1c02ab09dd87d
Christien <21901129@etu.unicaen.fr>
2021-03-12 16:22:48 +0100

    formulaire connexion

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,22 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
+import { action } from '@ember/object';
 
 export default class IndexRoute extends Route {
+  @service userAuth;
+
+  @action
+  login(model) {
+    this.store
+      .query('employee', { filter: { email: model.email } })
+      .then((data) => {
+        if (data.length) {
+          let user = data.firstObject;
+          if (user.password === model.password) {
+            this.userAuth.login(user);
+            this.transitionTo('board');
+          }
+        }
+      });
+  }
 }
